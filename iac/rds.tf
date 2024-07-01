957629c8fd6ea6dcaeed640a4b3e3e4fb419cf3a
tonnysina <92598332+tonnysina@users.noreply.github.com>
2024-07-01 22:58:50 +0200

    Update rds.tf

--- iac/rds.tf
+++ iac/rds.tf
@@ -4,7 +4,7 @@ resource "aws_db_instance" "rds_challenge_devops" {
   engine               = "postgres"
   engine_version       = var.db_engine_version
   instance_class       = var.db_instance_class
-  name                 = var.db_name
+  db_name                 = var.db_name
   username             = var.db_username
   password             = var.db_password
   skip_final_snapshot  = true
