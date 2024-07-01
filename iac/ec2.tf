0c0d3971800f42cdb86b0371dc9dfb86cddcf3ba
tonnysina <92598332+tonnysina@users.noreply.github.com>
2024-07-01 22:07:23 +0200

    Update ec2.tf

--- iac/ec2.tf
+++ iac/ec2.tf
@@ -1,1 +1,11 @@
+resource "aws_instance" "ec2_challenge_devops" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  root_block_device {
+    volume_size = 30
+  }
+  tags = {
+    Name = "ec2-challenge-devops"
+  }
+}
 
