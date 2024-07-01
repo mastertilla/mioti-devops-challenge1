18a3804eecca1761e54aea25005d6e68886011e8
tonnysina <92598332+tonnysina@users.noreply.github.com>
2024-07-01 22:06:38 +0200

    Update rds.tf

--- iac/rds.tf
+++ iac/rds.tf
@@ -1,1 +1,16 @@
+resource "aws_db_instance" "rds_challenge_devops" {
+  allocated_storage    = 10
+  storage_type         = "gp2"
+  engine               = "postgres"
+  engine_version       = var.db_engine_version
+  instance_class       = var.db_instance_class
+  name                 = var.db_name
+  username             = var.db_username
+  password             = var.db_password
+  skip_final_snapshot  = true
+  publicly_accessible  = true
+  tags = {
+    Name = "rds-challenge-devops"
+  }
+}
 
